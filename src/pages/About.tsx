cb43241a8ee89d2c578db9aef1ac86c861071720
katepad <katepad2020@gmail.com>
2025-03-11 10:55:06 -0700

    Global CSS & About Us Page Updates

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,27 @@
+
+import '../styles/Global.css'; 
+import '../styles/About.css';
+
 const About = () => {
     return (
       <div>
-        <h1>About Us</h1>
-        <p>We are building a simple React app!</p>
+        <h1 className = "page-h1"> About Us </h1>
+        <History />
       </div>
     );
   };
+
+const History = () => {
+  return (
+    <section className = "about-history-section">
+      <div className = "about-history-picture">
+      </div>
+      <div className = "about-history-content">
+        <h2 className = "page-h2"> History </h2>
+        <p className = "page-p"> Blah blah blah. </p>
+      </div>
+    </section>
+  );
+};
   
   export default About;
-  
