026e858716067c5c87e520bf84c187b5bc01afb8
katepad <katepad2020@gmail.com>
2025-03-17 16:42:18 -0700

    Updated contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,6 +6,7 @@ const Contact = () => {
     <>
       <h1 className="page-h1 color-purple" style={{ marginLeft: "5rem" }}> Contact Us </h1>
       <Form />
+      <Testimonial />
     </>
   );
 };
@@ -21,36 +22,65 @@ const Form = () => {
 
       <div className = "contact-form-div">
         
-        <h3 className = "page-h3 color-darkpurple"> We’d love to hear from you! We welcome every conversation with open ears, open hearts, and open minds. </h3>
+        <h3 className = "contact-h3 color-darkpurple"> We’d love to hear from you! We welcome every conversation with open ears, open hearts, and open minds. </h3>
         
-        <form action="https://api.web3forms.com/submit" method="POST" className = "contact-block">
+        <form action="https://api.web3forms.com/submit" method="POST" className = "contact-form">
           
           <input type="hidden" name="access_key" value="37a2d8e8-b510-4f39-b7d3-0348970fd5f3"/>
 
-          <p className = "contact-p color-purple"> Name: </p>
-          <input type = "text" name = "name" className = "contact-inputs" required/>
+          <div className = "form-left-side">
+            <p className = "contact-p color-purple"> Name: </p>
+            <input type = "text" name = "name" className = "contact-inputs" required/>
 
-          <p className = "contact-p color-purple"> E-mail: </p>
-          <input type = "email" name = "email" className = "contact-inputs" required/>
+            <p className = "contact-p color-purple"> E-mail: </p>
+            <input type = "email" name = "email" className = "contact-inputs" required/>
+
+            <br/>
+            <button className = "contact-submit-button" type = "submit"> Submit </button>
+          </div>
+
+          <div className = "form-right-side">
 
           <p className = "contact-p color-purple"> Subject / Reason: </p>
           <select name = "subject" className = "contact-inputs" required>
-            <option value="volvo">Question</option>
-            <option value="fiat">Event</option>
-            <option value="audi">Collaboration</option>
+            <option value = "General Inquiry"> General Inquiry </option>
+            <option value = "Event Inquiry"> Event Inquiry </option>
+            <option value = "Collab Inquiry"> Collaboration Inquiry </option>
+            <option value = "Submit Testimonial" > Submit Testimonial</option>
+            <option value = "Other"> Other </option>
           </select>
 
           <p className = "contact-p color-purple"> Message: </p>
-          <textarea name = "Message" className = "contact-inputs" required></textarea>
+          <textarea name = "Message" className = "contact-inputs msg-input" required></textarea>
+          </div>
 
-          <br/>
-          <button className = "contact-submit-button" type = "submit"> Submit </button>
         </form>
       </div>
     </section>
   );
 };
 
-  
+const Testimonial = () => {
+  return (
+    <section className= {"contact-testimonial-section section-purple"} >
+      <div className="button-div">
+        <button className = "arrow-button-left"> ♡ </button>
+      </div>
+      <div className = "contact-testimonial-content-div">
+        <h2 className = "contact-h2 color-green"> Member Testimonials </h2>
+        <p className = "page-p color-white"> In the heart of London, in a cozy little town called Sanrioville, there lived a bright and curious kitten named Hello Kitty. From a young age, she was fascinated by puzzles, patterns, and how things worked. While her friends enjoyed baking and playing outside, Kitty spent hours tinkering with her toy robot, trying to make it walk on its own. </p>
+        <br/>
+        <p className = "page-p color-white">  One day, her school announced a coding competition where students had to create a program that solved a real-world problem. Kitty was thrilled! She had never written a single line of code before, but that didn’t stop her. Determined, she rushed home and searched for beginner coding tutorials on her computer. </p>
+      </div>
+      <div>
+        <img className = "contact-testimonial-img" src="/assets/titocTemp.jpg" alt="First GBM with Harmony Hacks Founders" />
+      </div>
+      <div className="button-div">
+        <button className = "arrow-button-right"> ♡ </button>
+      </div>
+    </section>
+  );
+}
+
 export default Contact;
   
