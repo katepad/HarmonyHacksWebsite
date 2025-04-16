a3ba99d0a6602d882e85483da74d4a4b118d098f
cherishma23 <115597751+cherishma23@users.noreply.github.com>
2025-04-15 17:08:03 -0700

    Reverted code

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,40 +1,29 @@
 import '../styles/Global.css';
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import contactData from "../Data/Testimonials.json";
 
 type Testimonial = {
   testimonial_id: number;
-  testimonial_name: string;
+  testimonial_firstName: string;
+  testimonial_lastName: string;
   testimonial_description: string;
   testimonial_image: string;
 };
 
 const Testimonial = () => {
-  const [testimonial, setTestimonial] = useState<Testimonial[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    fetch("/data/Testimonials.json")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Loaded Board JSON:", data);
-        setTestimonial(data);
-      })
-      .catch((err) => console.error("Failed to load testimonial JSON:", err));
-  }, []);
-
-  if (testimonial.length === 0) return null;
-
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonial.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % contactData.length);
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => 
-      prevIndex === 0 ? testimonial.length - 1 : prevIndex - 1
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? contactData.length - 1 : prevIndex - 1
     );
   };
 
-  const member = testimonial[currentIndex];
+  const member: Testimonial = contactData[currentIndex];
 
   return (
     <section className="contact-testimonial-section section-purple">
@@ -45,14 +34,18 @@ const Testimonial = () => {
       <div className="contact-testimonial-content-div">
         <div className="contact-testimonial-text-div">
           <h2 className="contact-h2 color-green"> Member Testimonials </h2>
-          <h3 className="page-h3 color-white">{member.testimonial_name}</h3>
-          <p className="contact-testimonial-p color-white">{member.testimonial_description}</p>
+          <h3 className="page-h3 color-white">
+            {member.testimonial_firstName} {member.testimonial_lastName}
+          </h3>
+          <p className="contact-testimonial-p color-white">
+            {member.testimonial_description}
+          </p>
         </div>
         <div className="contact-testimonial-div">
-          <img 
-            className="contact-testimonial-img" 
-            src={member.testimonial_image || "/assets/temp.png"} 
-            alt={`${member.testimonial_name}`} 
+          <img
+            className="contact-testimonial-img"
+            src={member.testimonial_image || "/assets/temp.png"}
+            alt={`${member.testimonial_firstName} ${member.testimonial_lastName}`}
           />
         </div>
       </div>
