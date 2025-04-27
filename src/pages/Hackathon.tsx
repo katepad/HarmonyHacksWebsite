43dbf20ef5a49004666094a4d44bf1c45b24cd8e
katepad <katepad2020@gmail.com>
2025-04-26 23:06:17 -0700

    added images and changed website content

--- src/pages/Hackathon.tsx
+++ src/pages/Hackathon.tsx
@@ -31,9 +31,10 @@ const Guide = () => {
     return (
       <section className= "hackathon-guide-section" >
         <div>
-            <h2 className = "page-h2 color-purple">♡ Our Process </h2>
-            <h3 className = "page-h3 color-darkpurple"> Creating a Hackathon event was no easy task... add more here </h3>
-            
+            <h2 className = "page-h2 color-purple">♡ Our Process </h2> 
+            <h3 className = "page-h3 color-darkpurple"> Creating our Harmony Hackathons was no easy task, but thanks to the dedication of our mentors and faculty members, we were able to bring our vision to life. This guide shares the process we followed. From forming a club and building a community, to organizing outreach, preparing materials, and running the event itself. </h3>
+            <br/>
+
             <VerticalTimeline lineColor = "#633160" layout ={'1-column-left'}>
 
             {hackathonGuide.map((member: HackathonSteps) => (
@@ -41,14 +42,15 @@ const Guide = () => {
                 <VerticalTimelineElement className = "vertical-timeline-element"
                 
                 contentStyle={{ background: "#fff2fe", color: '#633160' }}
-                iconStyle={{background:"#633160", color:"#fff2fe"}}
+                icon = {member.id}
+                iconStyle={{background:"#633160", color:"#fff2fe", fontFamily: "Ubuntu", textAlign: "center", fontSize: "1.5rem"}}
                 >
                 <div className = "hackathon-guide-content-div">
                     <div>
                     <h3 className = "page-h3 color-darkpurple"> {member.hackathonStep_title} </h3>
                     <p className = "page-p color-purple">{member.hackathonStep_description}</p>
                     </div>
-                    <div>
+                    <div className="hackathon-guide-img-div">
                     <img className="hackathon-guide-img" src={member.hackathonStep_image || "/assets/temp.png"} alt={`${member.hackathonStep_title}`} />
                     </div>
                 </div>
